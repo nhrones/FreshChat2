f6dabc3171f2af49ccbbc761a1f01cb469bbfbc9
Nick Hrones <nhrones@users.noreply.github.com>
2022-05-13 19:21:08 -0400

    first commit

---
+++ ctx.ts
@@ -0,0 +1,19 @@
+
+export const DISCONNECTED = "🔴 Disconnected";
+export const CONNECTING = "🟡 Connecting...";
+export const CONNECTED = "🟢 Connected";
+
+export const welcome = `
+This application sends your chat messages
+live across the world using a global
+'BroadcastChannel' in Deno Deploy.
+
+`
+export const askForName = `Before you can chat, we need a username.
+`
+export interface Message {
+    id: string;
+    ts: string;
+    user: string;
+    body: string;
+  }
